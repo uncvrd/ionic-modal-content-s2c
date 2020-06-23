ca245209701f500ad527fef19ef70e06c68aeba9
Jordan Lewallen <support@uncvrd.co>
2020-06-23 00:56:53 -0700

    init commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonModal,  } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <>
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Blank</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <IonHeader collapse="condense">
+                        <IonToolbar>
+                            <IonTitle size="large">Blank</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+                    <ExploreContainer />
+                </IonContent>
+            </IonPage>
+            <IonModal
+                isOpen={true}
+                swipeToClose
+                mode="ios"
+            >
+              {/* Remove this IonContent and you'll be able to swipe to close */}
+                <IonContent>
+
+                </IonContent>
+            </IonModal>
+        </>
+    );
 };
 
 export default Home;
